57d473a58ebc217a197c5e7cf80735e9495e9857
Purusottam <purusottamadhikari234@gmail.com>
2021-03-07 12:46:53 +0545

    configurations

--- demo/settings.py
+++ demo/settings.py
@@ -14,8 +14,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'core',
-    'django_rename_app'
+
+
 ]
 
 MIDDLEWARE = [
@@ -76,3 +81,10 @@ if ENVIRONMENT == 'production':
     SECURE_REDIRECT_EXEMPT = []
     SECURE_SSL_REDIRECT = True
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+SITE_ID = 1

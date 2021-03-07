fae1b68678fc63d2628319a6e30c23405c57441e
Purusottam <purusottamadhikari234@gmail.com>
2021-03-07 14:38:01 +0545

    editing templates

--- demo/urls.py
+++ demo/urls.py
@@ -7,11 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts', include('allauth.urls')),
     path('', include('core.urls', namespace='core')),
-]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
